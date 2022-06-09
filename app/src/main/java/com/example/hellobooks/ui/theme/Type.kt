e997d4f81fe7ad1a66c1170ea65af29c216f3938
Kamidro6530 <daniel6530@wp.pl>
2022-06-09 12:42:50 +0200

    Added and set new font

--- app/src/main/java/com/example/hellobooks/ui/theme/Type.kt
+++ app/src/main/java/com/example/hellobooks/ui/theme/Type.kt
@@ -2,15 +2,28 @@ package com.example.hellobooks.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.hellobooks.R
+
+val roboto_fonts = FontFamily(
+    Font(R.font.roboto_bold, weight = FontWeight.Bold), // Pogrubiona
+    Font(R.font.roboto_regular, weight = FontWeight.Normal) , //Klasyczna czcionka
+    Font(R.font.roboto_medium, weight = FontWeight.Medium) , // Lekko pogrubiona
+    Font(R.font.roboto_black, weight = FontWeight.Black) , //Podobny jak bold pogrubiona czcionka
+    Font(R.font.roboto_italic), //Pochylona
+    Font(R.font.roboto_light, weight = FontWeight.Light) , //Chudsza czcionka między Thin a regular
+    Font(R.font.roboto_thin, weight = FontWeight.Thin) , //Bardzo cienki do podpisów/wyjaśnień
+
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = roboto_fonts,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
