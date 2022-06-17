6db483ba270e3abcd4be70e7e620f4ebc6c52dc3
Kamidro6530 <daniel6530@wp.pl>
2022-06-17 11:08:50 +0200

    Added MVVM architecture

---
+++ app/src/main/java/com/example/hellobooks/mvvm/BookViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.hellobooks.mvvm
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hellobooks.book.Book
+import com.example.hellobooks.repository.BookRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class BookViewModel( val bookRepository: BookRepository) : ViewModel() {
+
+    private var _listOfBooks = MutableStateFlow<List<Book>>(mutableStateListOf<Book>())
+    val listOfBooks = _listOfBooks
+
+
+    init {
+        viewModelScope.launch {
+            listOfBooks.value = bookRepository.bookDao.getAllBooks()
+
+        }
+    }
+
+    fun insertBook(book: Book) {
+        viewModelScope.launch { bookRepository.insertBook(book)  }
+    }
+
+    fun deleteBook(book: Book){
+        viewModelScope.launch { bookRepository.deleteBook(book) }
+    }
+
+
+
+
+}
